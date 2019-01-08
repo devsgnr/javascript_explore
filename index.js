a7dfbb0079f99e726e78df45f56740a164922890
itguru190 <itguru190@gmail.com>
2019-01-08 22:57:22 +0100

    Commit (#4)

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
+'use strict';
+
+/*
+	* Using Function Objects
+	*
+	* Returns the {Person} Object
+	*
+	* @param {string} name - Name of person
+	* @param {number} yearOfBirth - Year of Birth of Person
+	* @return {object} Person - Returns new Person object
+	*
+*/
 function Person(name, yearOfBirth) {
 	this.name = name;
 	this.yearOfBirth = yearOfBirth;
-	this.year = new Date().getFullYear();
-	this.age = this.year - this.yearOfBirth;
+	this.age = new Date().getFullYear() - this.yearOfBirth;
 	this.dateJoined = new Date();
 	this.allowAccess = this.age >= 18 ? true : false;
+	this.subscribed = true;
+
+	this.sayName = function() {
+		return this.name;
+	};
 }
 
-var johnDoe = new Person('John Doe', 1988),
-	jamesDoe = new Person('James Doe', 2004),
-	janeDoe = new Person('Jane Doe', 1960);
+/*
+	* Using ES6 Classes
+	*
+	* Returns the {User} Object
+	*
+	* @param {string} name - Name of User
+	* @param {number} yearOfBirth - Year of Birth of User
+	* @return {object} User - Returns new User object
+	*
+*/
+class User {
+	constructor(name, yearOfBirth) {
+		this.name = name;
+		this.yearOfBirth = yearOfBirth;
+		this.age = new Date().getFullYear() - yearOfBirth;
+		this.dateJoined = new Date();
+		this.allowAccess = this.age >= 18 ? true : false;
+		this.subscribed = true;
+	}
 
-console.table([ johnDoe, jamesDoe, janeDoe ]);
+	sayName() {
+		return this.name;
+	}
+}
+
+var janeDoe = new User('Jonathan James', 1998);
+var johnDoe = new Person('Emmanuel Watila', 1999);
+
+console.table([ johnDoe, janeDoe ]);
+
+/*
+	* Chek function for mocha & chai testing
+	*
+ 	* @param {type} to be checked
+	* @return {boolean} result of check
+ */
+function checkTypeStr(type) {
+	let result = typeof type;
+	return result;
+}
---
+++ test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+mocha.setup('bdd');
+let assert = chai.assert;
+
+// Testing
+describe('typeCheck', function() {
+	//Check if string values are {true}
+	describe('stringCheck', function() {
+		it('should be a string', function() {
+			assert.equal(checkTypeStr(Person.name), 'string');
+		});
+
+		it('should be a string', function() {
+			assert.equal(checkTypeStr(User.name), 'string');
+		});
+	});
+});
+
+mocha.run();
