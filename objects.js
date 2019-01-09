e33ed7be5128836a74135949f4a831f4df36a617
itguru190 <itguru190@gmail.com>
2019-01-09 13:59:37 +0100

    Commit(#6)

--- objects.js
+++ objects.js
@@ -1,6 +1,20 @@
 'use strict';
 
 let user = new Object();
-user = {};
+
+user = {
+	name: 'Emmanuel Watila',
+	yob: 1999,
+	username: 'itguru190',
+	password: '',
+	hashPassword: function(password) {
+		/* Function for hashing password : more things to be added*/
+		this.password = password;
+	},
+	playlist: 0,
+	subscribedToPro: false
+};
+
+user.hashPassword('emmanuel99');
 
 console.log(user);
