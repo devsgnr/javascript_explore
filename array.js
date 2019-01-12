a4ab70e9fe6f29b0712fa0f56724ab7e640966f5
itguru190 <itguru190@gmail.com>
2019-01-12 15:24:49 +0100

    commit(#14) - modified

--- array.js
+++ array.js
@@ -19,8 +19,9 @@ console.log(name.slice(0, 7));
 
 // .concat -
 let arrayLike = {
-	0: 'something',
-	1: 'else',
+	0: 'are',
+	1: 'something',
+	2: 'else',
 	// Symbol.isConcatSpreadable - if "true"
 	// enables object to be copied as an array
 	// giving the output
@@ -52,3 +53,78 @@ let users = [ { id: 1, name: 'Emmanuel' }, { id: 2, name: 'Jonathan' } ];
 let user = users.findIndex((item) => item.name == 'Emmanuel');
 
 console.log(user.name);
+
+// .map - it calls the function for each element of the array
+// and returns the array results.
+let people = arr.map((item) => item.length);
+//console.log(people);
+
+//.sort - reorder the arrangements in an array
+// we can pass in "ordering function" as an
+// argument....
+
+function compareNumeric(a, b) {
+	if (a > b) return 1;
+	if (a == b) return 0;
+	if (a < b) return -1;
+}
+
+people.sort(compareNumeric);
+console.log(people);
+
+function checkMod(a) {
+	if (a % 2 == 0) return -1;
+	if (a == 2) return 0;
+	if (a % 2 != 0) return 1;
+}
+
+people.sort(checkMod);
+console.log(people);
+
+// .split - splits a string into an array followed
+// by a delimeter (string) which is the .split argument
+// .split (delim);
+//
+// NB : Delimeter should march delimeter used in string
+let names = 'John, Jane, Mike, Doe',
+	namesArr = names.split(', ');
+
+for (let name of namesArr) {
+	console.log(`A message to ${name}`);
+}
+
+// .join - does the opposite of .split
+// It creates a string from an array of elements
+// and glues them by the seperator provided
+let joined = arr.join('::');
+console.log(joined);
+
+// .reduce / .reduceRight - are used to caluculate a single
+// value based on the array
+
+let totalLen = people.reduce((sum, current) => sum + current, 0);
+console.log(totalLen);
+
+let totalLen2 = people.reduceRight((sum, current) => sum + current);
+console.log(totalLen2);
+
+// Array.isArray - arrays and object aren't distinguishable in the language
+// so "typeof" will return "object" for both an array & an object
+// so Array.isArray is used to determine the existance of an array
+
+console.log(Array.isArray(arrayLike));
+console.log(Array.isArray(people));
+
+// thisArg
+
+let useR = {
+	age: 18,
+	younger(otherUser) {
+		return otherUser.age < this.age;
+	}
+};
+
+let userS = [ { age: 12 }, { age: 16 }, { age: 32 } ];
+let youngsters = userS.filter(useR.younger, useR);
+
+console.log(youngsters.length);
